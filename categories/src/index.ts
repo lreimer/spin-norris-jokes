5af7d74c7a19ba9566ae7ab779ddbd61d3b474ba
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2024-12-12 23:30:12 +0100

    Added first simple proxy implementation.

--- categories/src/index.ts
+++ categories/src/index.ts
@@ -1,6 +1,10 @@
 import { ResponseBuilder } from "@fermyon/spin-sdk";
 
 export async function handler(req: Request, res: ResponseBuilder) {
-    console.log(req);
-    res.send("hello universe");
+    const apiUrl = `https://api.chucknorris.io/jokes/categories`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    console.log(`Received categories ${JSON.stringify(data)}`);
+    res.send(data);
 }
--- random/src/index.ts
+++ random/src/index.ts
@@ -1,6 +1,13 @@
 import { ResponseBuilder } from "@fermyon/spin-sdk";
 
 export async function handler(req: Request, res: ResponseBuilder) {
-    console.log(req);
-    res.send("hello universe");
+    const requestUrl = new URL(req.url);
+    const category = requestUrl.searchParams.get('category') || '';
+
+    const apiUrl = `https://api.chucknorris.io/jokes/random`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    console.log(`Received random joke ${JSON.stringify(data)}`);
+    res.send(data.value);
 }
