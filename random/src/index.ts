695522942ab47ac52b8c3531a08ff22e6d349524
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2024-12-12 23:57:24 +0100

    Implemented 404 joke not found.

--- random/src/index.ts
+++ random/src/index.ts
@@ -4,10 +4,21 @@ export async function handler(req: Request, res: ResponseBuilder) {
     const requestUrl = new URL(req.url);
     const category = requestUrl.searchParams.get('category') || '';
 
-    const apiUrl = `https://api.chucknorris.io/jokes/random`;
+    var apiUrl = '';
+    if (category === '') {
+        apiUrl = `https://api.chucknorris.io/jokes/random`; 
+    } else {
+        apiUrl = `https://api.chucknorris.io/jokes/random?category=${category}`;
+    }
+
     const response = await fetch(apiUrl);
     const data = await response.json();
 
     console.log(`Received random joke ${JSON.stringify(data)}`);
+    if (data.status === 404) {
+        res.status(404);
+        res.send('No joke found.');
+        return;
+    }
     res.send(data.value);
 }
