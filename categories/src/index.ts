ccc4f5043e764fee1177af8d1f60ac20659dc848
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2024-12-13 09:42:59 +0100

    Added category caching to default K/V.

--- categories/src/index.ts
+++ categories/src/index.ts
@@ -1,10 +1,31 @@
-import { ResponseBuilder } from "@fermyon/spin-sdk";
+import { ResponseBuilder, Kv } from "@fermyon/spin-sdk";
+
+const CATEGORY_KEY = "categories";
 
 export async function handler(req: Request, res: ResponseBuilder) {
+    let store = Kv.openDefault();
+
+    const requestUrl = new URL(req.url);
+    const refreshParam = requestUrl.searchParams.get('refresh');
+    const refresh = refreshParam === 'true';
+    if (refresh) {
+        console.log("Refresh flag set. Deleting cached data.");
+        await store.delete(CATEGORY_KEY);
+    }
+    
+    if (store.exists(CATEGORY_KEY)) {
+        console.log("Categories already in default K/V store. Returning cached data.");
+        let categories = await store.getJson(CATEGORY_KEY);
+        res.send(categories);
+        return;
+    }
+
+    console.log("Fetching categories from Chuck Norris API.");
     const apiUrl = `https://api.chucknorris.io/jokes/categories`;
     const response = await fetch(apiUrl);
-    const data = await response.json();
+    const categories = await response.json();
 
-    console.log(`Received categories ${JSON.stringify(data)}`);
-    res.send(data);
+    console.log("Storing categories in default K/V store.");
+    await store.setJson(CATEGORY_KEY, categories);
+    res.send(categories);
 }
